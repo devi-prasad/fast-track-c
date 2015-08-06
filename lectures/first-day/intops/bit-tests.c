6b0d0f105d048e4c3e83909587909f60d8a14dff
dprasad <dprasadm@gmail.com>
2015-08-06 17:23:18 +0530

    cosmetic changes - replaced tabs by spaces

--- lectures/first-day/intops/bit-tests.c
+++ lectures/first-day/intops/bit-tests.c
@@ -19,15 +19,15 @@ void run_byte_tests()
     unsigned char x = 1;
     
     /* positive numbers */
-	assert(x << 1 == 2);
-	assert(x << 2 == 4);
-	assert(x << 3 == 8);
+    assert(x << 1 == 2);
+    assert(x << 2 == 4);
+    assert(x << 3 == 8);
 	
-	assert(x << 4 == 16);
-	assert(x << 4 == 0x0F + 1);
-	assert(x << 4 == 0x10);
+    assert(x << 4 == 16);
+    assert(x << 4 == 0x0F + 1);
+    assert(x << 4 == 0x10);
 
-	assert(x << 7 == 128);
+    assert(x << 7 == 128);
     assert(x << 7 == 0x7F + 1);
     assert(x << 7 == 0x80);
 
@@ -35,25 +35,25 @@ void run_byte_tests()
     assert(x << 7 > 0);
     assert((x << 7) >> 7 == 1);
 
-	assert(((x << 7) | 0x7F) == 0xFF);
-	assert(((x << 7) | 0x7F) == UINT8_MAX);    
+    assert(((x << 7) | 0x7F) == 0xFF);
+    assert(((x << 7) | 0x7F) == UINT8_MAX);    
 }
 
 
 void run_signed_int_bit_tests()
 {
-	int x = 1;
+    int x = 1;
     
     assert(sizeof(int) == 4); /* we assume ints to be 32-bit beasts! */
     
     /* positive numbers */
-	assert(x << 1 == 2);
-	assert(x << 2 == 4);
-	assert(x << 3 == 8);
-	assert(x << 4 == 16);
-	assert(x << 8 == 256);
-	assert(x << 16 == 65536);
-	assert(x << 16 == x << 8 * x << 8);
+    assert(x << 1 == 2);
+    assert(x << 2 == 4);
+    assert(x << 3 == 8);
+    assert(x << 4 == 16);
+    assert(x << 8 == 256);
+    assert(x << 16 == 65536);
+    assert(x << 16 == x << 8 * x << 8);
     assert(x << 30 > 0);
 
     assert((x << 1) >> 1 == 1);
@@ -74,26 +74,26 @@ void run_signed_int_bit_tests()
     /* in order to understand why the following assertions hold, one needs
      * to know the two's complement binary number system.
      */
-	assert(x << 31 == INT_LEAST32_MIN);
-	assert(((x << 31) | 0x7FFFFFFF) == 0xFFFFFFFF);
-	assert((x << 31) >> 31 == -1);
-	assert((x << 31) >> 31 == 0xFFFFFFFF);
+    assert(x << 31 == INT_LEAST32_MIN);
+    assert(((x << 31) | 0x7FFFFFFF) == 0xFFFFFFFF);
+    assert((x << 31) >> 31 == -1);
+    assert((x << 31) >> 31 == 0xFFFFFFFF);
     assert((x << 31) >> 31 == ((x << 31) | 0x7FFFFFFF));
 }
 
 void run_unsigned_int_bit_tests()
 {
-	unsigned int x = 1;
+    unsigned int x = 1;
 
     assert(sizeof(int) == 4); /* we assume ints to be 32-bit beasts! */
     /* positive numbers */
-	assert(x << 1 == 2);
-	assert(x << 2 == 4);
-	assert(x << 3 == 8);
-	assert(x << 4 == 16);
-	assert(x << 8 == 256);
-	assert(x << 16 == 65536);
-	assert(x << 16 == x << 8 * x << 8);
+    assert(x << 1 == 2);
+    assert(x << 2 == 4);
+    assert(x << 3 == 8);
+    assert(x << 4 == 16);
+    assert(x << 8 == 256);
+    assert(x << 16 == 65536);
+    assert(x << 16 == x << 8 * x << 8);
     assert(x << 30 > 0);
 
     assert((x << 1) >> 1 == 1);
@@ -104,10 +104,10 @@ void run_unsigned_int_bit_tests()
     /* this looks perfect */    
     assert(x << 31 == x << 16 * x << 15);
     assert(x << 31 > 0);
-	assert((x << 31) >> 31 == 1);
+    assert((x << 31) >> 31 == 1);
 
-	assert(((x << 31) | 0x7FFFFFFF) == 0xFFFFFFFF);
-	assert(((x << 31) | 0x7FFFFFFF) == UINT32_MAX);
+    assert(((x << 31) | 0x7FFFFFFF) == 0xFFFFFFFF);
+    assert(((x << 31) | 0x7FFFFFFF) == UINT32_MAX);
 }
 
 void run_bit_tests()
@@ -115,3 +115,4 @@ void run_bit_tests()
     run_unsigned_int_bit_tests();
     run_signed_int_bit_tests();
 }
+
